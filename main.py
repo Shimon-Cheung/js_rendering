8a75fe3ecb648a3698996e454a01d0d89a465780
xmzhang.abcft <xmzhang@abcft.com>
2021-03-26 15:42:45 +0800

    【update】优化了目录结构

--- main.py
+++ main.py
@@ -27,44 +27,46 @@ async def read_root(item: Item):
     browser = await launch(
         {
             # 无头模式
-            'headless': True,
+            "headless": False,
             # 忽略 Https 报错信息
-            'ignoreHTTPSErrors': True,
+            "ignoreHTTPSErrors": True,
             # 防止多开导致的假死
-            'dumpio': True,
+            "dumpio": True,
             # 用户目录，用来储存临时缓存
-            'userDataDir': "./cache",
-            'args': [
+            "userDataDir": "./cache",
+            "args": [
                 # 关闭掉正在控制的提示
-                '--disable-infobars',
+                "--disable-infobars",
                 # 设置浏览器窗口最大化
-                '--start-maximized',
+                "--start-maximized",
                 # 关闭沙盒模式
-                '--no-sandbox',
+                "--no-sandbox",
                 # 设置报错等级规避一大堆info信息
                 "--log-level=3",
                 # 允许跨域
                 "--disable-web-security",
                 # 设置UA请求头
-                '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                'Chrome/85.0.4183.121 Safari/537.36',
+                "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/85.0.4183.121 Safari/537.36",
                 # 添加代理，暂时不开启
                 # "--proxy-server=http://127.0.0.1:80"
             ],
             # pyppeteer 异常处理 ValueError: signal only works in main thread
-            'handleSIGINT': False,
-            'handleSIGTERM': False,
-            'handleSIGHUP': False,
+            "handleSIGINT": False,
+            "handleSIGTERM": False,
+            "handleSIGHUP": False,
         }
     )
     # 打开一个页面
     page = await browser.newPage()
     # 设置页面的大小
-    await page.setViewport({'width': 1920, 'height': 1080})
+    await page.setViewport({"width": 1920, "height": 1080})
     # 开启js渲染
     await page.setJavaScriptEnabled(enabled=True)
     # 进行全局的js渲染，用来解决页面二次跳转
-    await page.evaluateOnNewDocument('function(){Object.defineProperty(navigator,"webdriver",{get:()=>undefined})}')
+    await page.evaluateOnNewDocument(
+        'function(){Object.defineProperty(navigator,"webdriver",{get:()=>undefined})}'
+    )
     # 打开链接
     await page.goto(item_dict["url"])
     # 用来等待渲染结果
@@ -74,5 +76,5 @@ async def read_root(item: Item):
     return HTMLResponse(page_text)
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8050)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8050)
