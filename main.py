ad07b0d60b97e65bf0a182954395722983f31f5f
xmzhang.abcft <xmzhang@abcft.com>
2021-04-14 14:34:40 +0800

    添加一个参数

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from pydantic import BaseModel
 from starlette.responses import HTMLResponse
 
 app = FastAPI()
+
+
 # 创建数据模型
 
 
@@ -45,6 +47,8 @@ async def read_root(item: Item):
                 "--log-level=3",
                 # 允许跨域
                 "--disable-web-security",
+                # 进行全局的js渲染，用来解决页面二次跳转
+                "--enable-automation"
                 # 设置UA请求头
                 "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                 "Chrome/85.0.4183.121 Safari/537.36",
